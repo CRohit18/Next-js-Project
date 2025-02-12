f1a687da5ae115eb031909c90c24807ea97f5b63
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 <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>
2025-02-12 18:29:44 +0530

    Chapter 8 done.

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Kmfnme64AdHqFt+E4P+NenSjUpj/lmsj24Vjv3Wrhac=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
