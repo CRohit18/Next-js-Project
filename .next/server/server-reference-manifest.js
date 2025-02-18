d9d1b39bce93bdbc2ecb6907cc503ae5a4cd000e
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 <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>
2025-02-18 12:08:35 +0530

    Chapter 9 Completed

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UqvIMCosz4uSgRblPd5Ng4xFGQ+6pIXLwx0tDD0Bhcc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
